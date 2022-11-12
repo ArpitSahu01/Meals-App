1eed8e31f60387da185badfa20b39fab386c4358
arpit <arpitsahu789@gmail.com>
2022-11-12 15:13:19 +0530

    Made changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,42 @@
 import 'package:flutter/material.dart';
+import '../models/numbers.dart';
 
-void main(){
-runApp(MyApp());
-}
+void main() {}
 
-class MyApp extends StatelessWidget{
-
-  @override
-  Widget build(BuildContext context){
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-        theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-    ),
-      home: MyHomePage(),
+      title: 'Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
     );
   }
-
 }
 
-class MyHomePage extends StatefulWidget{
-
+class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-
+  State<StatefulWidget> createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends  State<MyHomePage>{
-
+class MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
-        title: Text('DeliMeals'),
+        title: const Text('My Calculator'),
       ),
-      body:  Center(
-        child: Text('Navigation Time!'),
+      body: GridView(
+        children: num.map((val){
+            return Container();
+          }).toList
+        ,
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: (MediaQuery.of(context).size.width)/4,
+          childAspectRatio: 1/1,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+        ),
       ),
     );
   }
