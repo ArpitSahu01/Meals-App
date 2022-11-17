625a87a3786285aea1aee2ab0ebc31969a8f0431
arpit <arpitsahu789@gmail.com>
2022-11-17 23:05:20 +0530

    route added through constructors

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -18,7 +18,7 @@ class CategoriesScreen extends StatelessWidget{
 
       children: DUMMY_CATEGORIES.map(
             (catData)=>CategoryItem(
-            catData.title, catData.color
+            catData.id,catData.title, catData.color
         ),
       ).toList(),
 
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key? key}) : super(key: key);
+
+  final String id;
+  final String title;
+
+  CategoryMealsScreen({required this.id,required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text('Recipe'),),
+      appBar: AppBar(title:Text(title),),
       body: Center(
         child: Text("These are my selected meals"),
       ),
