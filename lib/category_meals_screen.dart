af32f85ca206e57ccee131d35a20ca6db732881c
arpit <arpitsahu789@gmail.com>
2022-11-19 11:08:00 +0530

    navigation through named routes

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
 
-  final String id;
-  final String title;
-
-  CategoryMealsScreen({required this.id,required this.title});
+  // final String id;
+  // final String title;
+  //
+  // CategoryMealsScreen({required this.id,required this.title});
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context)?.settings.arguments as Map<String,String>;
+    final categoryTitle = routeArgs['title'] as String;
     return Scaffold(
-      appBar: AppBar(title:Text(title),),
+      appBar: AppBar(title:Text(categoryTitle),),
       body: Center(
         child: Text("These are my selected meals"),
       ),
