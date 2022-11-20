30f4ae0a452b3d0b3f55cacc74eb5533951546d4
arpit <arpitsahu789@gmail.com>
2022-11-20 18:05:57 +0530

    fetched data at meal screen from dummy data

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
 
@@ -12,11 +13,16 @@ class CategoryMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context)?.settings.arguments as Map<String,String>;
     final categoryTitle = routeArgs['title'] as String;
+    final categoryId = routeArgs['id'];
+    final categoryMeal = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(title:Text(categoryTitle),),
-      body: Center(
-        child: Text("These are my selected meals"),
-      ),
+      body: ListView.builder(itemBuilder: (ctx,index){
+              return Text(categoryMeal[index].title);
+      } ,itemCount: categoryMeal.length ,),
     );
   }
 }
